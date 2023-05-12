1a8c7fbc68b7bb0f86006ba460c6e49d11e9373c
JavierPA3 <91657168+JavierPA3@users.noreply.github.com>
2023-05-12 10:01:14 +0200

    Update ej2.py

--- TercerTrimestre/ExpresionesRegulares/EJ2/ej2.py
+++ TercerTrimestre/ExpresionesRegulares/EJ2/ej2.py
@@ -1,5 +1,6 @@
 """
-2. Programa que reciba una url y el nombre de una etiqueta html. Si la url es válida debe mostrar por la pantalla el contenido de cada etiqueta.
+Programa que reciba una url y el nombre de una etiqueta html. Si la url es válida debe mostrar por la pantalla el
+contenido de cada etiqueta.
 
 Ejemplo:
 
@@ -15,7 +16,8 @@ Número de etiquetas encontradas: 1
 
 La salida sería:
 
-This domain is for use in illustrative examples in documents. You may use this domain in literature without prior coordination or asking for permission.
+This domain is for use in illustrative examples in documents. You may use this domain in literature without prior
+coordination or asking for permission.
 
 
 <a href="https://www.iana.org/domains/example">More information...</a>
@@ -26,8 +28,6 @@ Autor: Javier Postigo Arévalo
 Fecha: 11/05/2023
 
 """
-
-
 
 import sys
 import requests
@@ -48,7 +48,9 @@ except requests.exceptions.RequestException:
 
 font_code = response.text
 
-title = re.search(f'<{tag}>(.*)</{tag}>', font_code)
-number_of_tags = len(re.findall(f'<{tag}>', font_code))
-print(title.group(1))
+tag_content = re.findall(rf'<{tag}>(.*?)</{tag}>', font_code, flags=re.DOTALL)
+number_of_tags = len(re.findall(rf'<{tag}>', font_code))
+for x in tag_content:
+    print(x)
+print("")
 print("Número de etiquetas encontradas:", number_of_tags)
